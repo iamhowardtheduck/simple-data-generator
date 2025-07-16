4e0538b3cb488335ae215df238cd38c7aa940a34
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-07-16 15:00:15 -0400

    Update host-docker.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/host-docker.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/host-docker.sh
@@ -7,16 +7,20 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-echo "[1/3] Running osquery-setup.sh..."
+echo "[1/4] Running osquery-setup.sh..."
 bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/osquery-setup.sh
-echo "[1/3] Completed osquery-setup.sh."
+echo "[1/4] Completed osquery-setup.sh."
 
-echo "[2/3] Running mysql-docker-deploy.sh..."
+echo "[2/4] Running mysql-docker-deploy.sh..."
 bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/mysql-docker-deploy.sh
-echo "[2/3] Completed mysql-docker-deploy.sh."
+echo "[2/4] Completed mysql-docker-deploy.sh."
 
-echo "[3/3] Running install-fim-chaos.sh..."
+echo "[3/4] Running install-fim-chaos.sh..."
 bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/install-fim-chaos.sh
-echo "[3/3] Completed install-fim-chaos.sh."
+echo "[3/4] Completed install-fim-chaos.sh."
+
+echo "[4/4] Running update /etc/hosts"
+bash simple-data-generator/scripts/instruqt-labs/Getting-Data-Ingested/update-etc_hosts.sh
+echo "[4/4] Completed install update-etc_hosts.sh"
 
 echo "✅ All scripts completed successfully."
