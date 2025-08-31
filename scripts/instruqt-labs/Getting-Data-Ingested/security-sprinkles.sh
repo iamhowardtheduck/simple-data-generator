ba6a8d47144b902338c7809652beaba8f6d39401
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2025-08-31 11:40:33 -0400

    Update security-sprinkles.sh

--- scripts/instruqt-labs/Getting-Data-Ingested/security-sprinkles.sh
+++ scripts/instruqt-labs/Getting-Data-Ingested/security-sprinkles.sh
@@ -9,5 +9,10 @@ fi
 echo "Loading Elastic Rules"
 curl -X PUT "http://localhost:30001/api/detection_engine/rules/prepackaged" -u "elastic:changme"  --header "kbn-xsrf: true" -H "Content-Type: application/json"  -d '{}'
 curl -X POST "http://localhost:30001/api/detection_engine/rules/_bulk_create" -u "elastic:changeme" --header "kbn-xsrf: true" -H "Content-Type: application/json" -d @/root/simple-data-generator/detection-rules/getting-data-ingested-security-sprinkles.json
+echo
+echo
+echo "Security sprinkles applied, Bon Appétit!!!"
+echo 
+echo
 
 
